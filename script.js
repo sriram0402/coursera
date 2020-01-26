eb1d73e2446f65d814ba9381ddc89f80bfda0937
sriram0402 <55201542+sriram0402@users.noreply.github.com>
2020-01-26 12:06:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["sri","jack","jim","ramesh"];
+    var names = ["sri","jack","jim","ramesh","jonny","neelima","johnson","ram","kiran"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
